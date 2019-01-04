5f734d741520bfb341f41df641a5e1b5aacbc06a
Ryan Flett <ryan.flett1@gmail.com>
2019-01-05 09:00:29 +1000

    Refactor and add comments.

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -1,74 +1,85 @@
+import argparse
 import boto3
 import json
 import random
 import string
-import sys
 
 ecs = boto3.client('ecs')
-service_name = sys.argv[1]
-environment = sys.argv[2]
 
-tags = [
-    {
-        'key': 'environment',
-        'value': environment
-    },
-    {
-        'key': 'source',
-        'value': service_name
-    }
-]
 
-# register a new task definition
-register_res = ecs.register_task_definition(
-    family=service_name,
-    networkMode='bridge',
-    containerDefinitions=json.load(open('deploy/container_definitions.json')),
-    cpu='256',
-    memory='256',
-    tags=tags
-)
-
-# check if service exists
-service_exists = False
-services = ecs.describe_services(
-    cluster=environment,
-    services=[service_name]
-)
-for _service in services.get('services'):
-    if _service.get('serviceName') == service_name:
-        service_exists = True
-
-# update or create service
-common_service_settings = {
-    'cluster': environment,
-    'desiredCount': 1,
-    'taskDefinition': service_name,
-    'deploymentConfiguration': {
-        'maximumPercent': 200,
-        'minimumHealthyPercent': 50
-    }
-}
-if service_exists:
-    ecs.update_service(
-        **common_service_settings,
-        service=service_name,
-        forceNewDeployment=True
+def service_exists(service: str, env: str) -> bool:
+    """ Check if a service exists """
+    services = ecs.describe_services(
+        cluster=env,
+        services=[service]
     )
-else:
-    ecs.create_service(
-        **common_service_settings,
-        serviceName=service_name,
-        clientToken=''.join(random.choices(string.ascii_uppercase + string.digits, k=20)),
-        launchType='EC2',
-        placementStrategy=[
-            {
-                'type': 'spread',
-                'field': 'attribute:ecs.availability-zone'
-            },
-        ],
-        schedulingStrategy='REPLICA',
-        deploymentController={
-            'type': 'ECS'
+    for _service in services.get('services'):
+        if _service.get('serviceName') == service:
+            return True
+    return False
+
+
+if __name__ == '__main__':
+    # parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("service_name", help="The name of the service", type=str)
+    parser.add_argument("environment", help="The environment to deploy this service to", type=str)
+    args = parser.parse_args()
+
+    # resource tags
+    tags = [
+        {
+            'key': 'environment',
+            'value': args.environment
+        },
+        {
+            'key': 'source',
+            'value': args.service_name
         }
+    ]
+
+    # register a new task definition
+    register_res = ecs.register_task_definition(
+        family=args.service_name,
+        networkMode='bridge',
+        containerDefinitions=json.load(open('deploy/container_definitions.json')),
+        cpu='256',
+        memory='256',
+        tags=tags
     )
+
+    # common keyword args between create and update service functions
+    common_service_kwargs = {
+        'cluster': args.environment,
+        'desiredCount': 1,
+        'taskDefinition': args.service_name,
+        'deploymentConfiguration': {
+            'maximumPercent': 200,
+            'minimumHealthyPercent': 50
+        }
+    }
+
+    # update or create service
+    if service_exists(args.service_name, args.environment):
+        ecs.update_service(
+            **common_service_kwargs,
+            service=args.service_name,
+            forceNewDeployment=True
+        )
+    else:
+        ecs.create_service(
+            **common_service_kwargs,
+            serviceName=args.service_name,
+            clientToken=''.join(random.choices(string.ascii_uppercase + string.digits, k=20)),
+            launchType='EC2',
+            placementStrategy=[
+                {
+                    'type': 'spread',
+                    'field': 'attribute:ecs.availability-zone'
+                }
+            ],
+            schedulingStrategy='REPLICA',
+            deploymentController={
+                'type': 'ECS'
+            }
+        )
