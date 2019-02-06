09f298c8356fe56c9f4b77b7b0d69317051693c9
Ryan Flett <ryan.flett1@gmail.com>
2019-02-06 20:48:36 +1000

    update routes

--- src/app/routes.py
+++ src/app/routes.py
@@ -67,7 +67,7 @@ def signup():
     return SignupController.signup(request)
 
 
-@app.route('/user', methods=['POST'])
+@app.route('/users', methods=['POST'])
 @requires_jwt
 def user_create():
     """
@@ -77,7 +77,7 @@ def user_create():
     return UserController.user_create(request)
 
 
-@app.route('/user', methods=['PUT'])
+@app.route('/users', methods=['PUT'])
 @requires_jwt
 def update_user():
     """
@@ -97,7 +97,7 @@ def get_user(identifier):
     return UserController.user_get(identifier, request)
 
 
-@app.route('/org', methods=['POST'])
+@app.route('/orgs', methods=['POST'])
 @requires_jwt
 def org_create():
     """
