c33288440b29ea4743fe981a84289ea49c704a93
Ryan Flett <ryan.flett1@gmail.com>
2019-05-14 21:22:19 +1000

    Switch from scan to query for settings - closes #25

--- src/app/Controllers/SettingsController.py
+++ src/app/Controllers/SettingsController.py
@@ -1,14 +1,15 @@
 from app import user_settings_table, org_settings_table
 from app.Models import UserSetting, OrgSetting
-from boto3.dynamodb.conditions import Attr
+from boto3.dynamodb.conditions import Key
 
 
 class SettingsController(object):
     @staticmethod
     def get_user_settings(user_id: int) -> UserSetting:
         """ Returns user settings from DynamoDB as a UserSetting object """
-        settings_obj = user_settings_table.scan(
-            FilterExpression=Attr('user_id').eq(user_id)
+        settings_obj = user_settings_table.query(
+            Select='ALL_ATTRIBUTES',
+            KeyConditionExpression=Key('user_id').eq(user_id)
         ).get('Items')[0]
         return UserSetting(**settings_obj)
 
@@ -23,8 +24,9 @@ class SettingsController(object):
     @staticmethod
     def get_org_settings(org_id: int) -> OrgSetting:
         """ Returns org settings from DynamoDB as a UserSetting object """
-        settings_obj = org_settings_table.scan(
-            FilterExpression=Attr('org_id').eq(org_id)
+        settings_obj = org_settings_table.query(
+            Select='ALL_ATTRIBUTES',
+            KeyConditionExpression=Key('org_id').eq(org_id)
         ).get('Items')[0]
         return OrgSetting(**settings_obj)
 
