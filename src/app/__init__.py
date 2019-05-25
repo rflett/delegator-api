be650a6d835f282ab893c83bb7757399b91b58c1
Ryan Flett <ryan.flett1@gmail.com>
2019-05-25 14:38:50 +1000

    Enable profiling in envs except for Ci. Make int tests manual

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -135,5 +135,5 @@ def g_response(msg: typing.Optional[str] = None, status: int = 200, **kwargs) ->
 # routes
 from app import routes  # noqa
 
-if getenv('APP_ENV', 'Local') == 'Local':
+if getenv('APP_ENV') != 'Ci':
     flask_profiler.init_app(app)
