2250b327f184da65164027f2ce0df8f3382e0469
Ryan Flett <ryan.flett1@gmail.com>
2019-05-29 18:23:36 +1000

    DelayedTask now has delayed_by, expired and snoozed are nullable timestamps. Remove redundant ID.

--- src/app/Models/DelayedTask.py
+++ src/app/Models/DelayedTask.py
@@ -1,39 +1,44 @@
 import datetime
+import typing
 from app import db
 
 
 class DelayedTask(db.Model):
     __tablename__ = "tasks_delayed"
 
-    id = db.Column('id', db.Integer, primary_key=True)
     task_id = db.Column('task_id', db.Integer, db.ForeignKey('tasks.id'))
     delay_for = db.Column('delay_for', db.Integer)
-    delayed_at = db.Column('delayed_at', db.DateTime, default=datetime.datetime.utcnow)
-    snoozed = db.Column('snoozed', db.Boolean, default=False)
-    expired = db.Column('expired', db.Boolean, default=False)
+    # https://docs.sqlalchemy.org/en/13/faq/ormconfiguration.html#how-do-i-map-a-table-that-has-no-primary-key
+    delayed_at = db.Column('delayed_at', db.DateTime, default=datetime.datetime.utcnow, primary_key=True)
+    delayed_by = db.Column('delayed_by', db.Integer, db.ForeignKey('users.id'))
+    snoozed = db.Column('snoozed', db.DateTime, default=None)
+    expired = db.Column('expired', db.DateTime, default=None)
 
     tasks = db.relationship("Task")
+    users = db.relationship("User")
 
     def __init__(
             self,
             task_id: int,
             delay_for: int,
-            delayed_at: datetime,
-            snoozed: bool = False,
-            expired: bool = False
+            delayed_at: datetime.datetime,
+            delayed_by: int,
+            snoozed: typing.Union[datetime.datetime, None] = None,
+            expired: typing.Union[datetime.datetime, None] = None
     ):
         self.task_id = task_id
         self.delay_for = delay_for
-        self.delayed_at = delayed_at,
+        self.delayed_at = delayed_at
+        self.delayed_by = delayed_by
         self.snoozed = snoozed
         self.expired = expired
 
     def as_dict(self):
         return {
-            "id": self.id,
             "task_id": self.task_id,
             "delay_for": self.delay_for,
             "delayed_at": self.delayed_at,
+            "delayed_by": self.delayed_by,
             "snoozed": self.snoozed,
             "expired": self.expired
         }
