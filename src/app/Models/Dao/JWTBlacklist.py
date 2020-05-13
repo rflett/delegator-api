bbd053d66c50d20404da8bea36f255cd287f1d03
Ryan Flett <ryan.flett1@gmail.com>
2020-05-13 22:06:57 +1000

    Add JWT blacklist dao

---
+++ src/app/Models/Dao/JWTBlacklist.py
@@ -0,0 +1,19 @@
+from app.Extensions.Database import db
+
+
+class JWTBlacklist(db.Model):
+    __tablename__ = "jwt_blacklists"
+
+    jti = db.Column("jti", db.String, primary_key=True)
+    exp = db.Column("exp", db.Integer)
+
+    def __init__(self, jti: str, exp: int):
+        self.jti = jti
+        self.exp = exp
+
+    def as_dict(self) -> dict:
+        """dict repr of a JWTBlacklist object"""
+        return {
+            "jti": self.jti,
+            "exp": self.exp
+        }
